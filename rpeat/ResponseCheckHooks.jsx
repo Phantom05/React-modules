ab75aa4932a316535f95fe3242d6cb1b9ff4b198
Phantom05 <monster2jy@gmail.com>
2019-04-26 03:07:46 +0900

    UPDATE: react 영화예매 study

--- rpeat/ResponseCheckHooks.jsx
+++ rpeat/ResponseCheckHooks.jsx
@@ -1,30 +1,34 @@
-import React, { Component, memo } from 'react';
-
+import React, { Component, memo, useStat, useRef } from 'react';
+// 훅스에서는 this의 속성들을 ref로 표현함
 const ResponseCheckHooks = memo(() => {
   const [ state,setState ] = React.useState('waiting');
   const [ result,setResult ] = React.useState([]);
   const [ message,setMessage ] = React.useState('클릭하여 시작해주세요!');
-  ResponseCheckHooks.timeOut;
-  ResponseCheckHooks.startTime;
-  ResponseCheckHooks.endTime;
+  // ResponseCheckHooks.timeOut; 요로쿰 은 안씀
+  // ResponseCheckHooks.startTime;
+  // ResponseCheckHooks.endTime;
+  const timeout = useRef(null);
+  const startTime = useRef();
+  const endTime = useRef();
+
   const hooksOnclick  =(e)=>{
     if (state == 'waiting') {
       setState('ready');
       setMessage('초록색이 되면 클릭하세요!');
-      ResponseCheckHooks.timeOut = setTimeout(() => {
+      timeout.current = setTimeout(() => {
           setState('now');
           setMessage('지금 클릭하세요!');
-          ResponseCheckHooks.startTime= new Date();
+          startTime.current= new Date();
         }, Math.floor(Math.random() * 1000) + 2000)
     } else if (state === 'ready') {
       clearTimeout(ResponseCheckHooks.timeOut);
       setState('waiting');
       setMessage('너무 성급하시군요! 초록색이 된 후에 클릭하세요')
     } else if (state === 'now') {
-      ResponseCheckHooks.endTime = new Date();
+      endTime.current = new Date();
       setState('waiting')
       setMessage('클릭해서 시작하세요.')
-      setResult((prevState)=>[...result, ResponseCheckHooks.endTime - ResponseCheckHooks.startTime])
+      setResult((prevState)=>[...result, endTime.current - startTime.current])
     }
   }
 
