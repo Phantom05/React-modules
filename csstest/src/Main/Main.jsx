e53e1b81c548b5ae3c0c1bb1be201a14941cdc41
Phantom05 <monster2jy@gmail.com>
2019-05-08 06:09:08 +0900

    Update: React css testing

--- csstest/src/Main/Main.jsx
+++ csstest/src/Main/Main.jsx
@@ -1,13 +1,25 @@
 import React, {Component} from 'react';
 import "./Main.scss";
+import '../About/About';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
+const MenuItem = ({active, children, to}) => (
+  <div className="menu-item">
+          {children}
+  </div>
+)
 class Main extends Component{
   render(){
     return(
       <>
         <h1 className="Main1">
-          Main Hello ㅠㅠ 흠..
+          Main Hello, React
         </h1>
+        <div className="menu">
+          <MenuItem title="홈" />
+          <MenuItem title="소개" />
+          <MenuItem title="홈포스트" />
+        </div>
       </>
     )
   }
