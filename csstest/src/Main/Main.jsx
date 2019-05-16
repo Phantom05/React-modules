66304b5614953a529293171de37d1a0ba1978785
Phantom05 <monster2jy@gmail.com>
2019-05-16 22:54:42 +0900

    UPDATE : 동원 예비군 끝~ 오늘은 쉬자 ㅠㅠ

--- csstest/src/Main/Main.jsx
+++ csstest/src/Main/Main.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import "./Main.scss";
 import '../About/About';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
 
 const MenuItem = ({active, children, to}) => (
   <div className="menu-item">
